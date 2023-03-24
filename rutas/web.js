7ac6618a3e47ac3a37d65195806e2709566f293d
hdisra318 <hdisra318@ciencias.unam.mx>
2023-03-23 22:39:10 -0600

    Agregando peticones para el controlador

--- rutas/web.js
+++ rutas/web.js
@@ -5,4 +5,17 @@ router.get('/', (peticion, respuesta) => {
     respuesta.send('Hola mundo')
 })
 
+// Peticiones
+router.post('/publicaciones/:id/:titulo/:autores/:fecha', (peticion, respuesta) => {
+    controladorPublicaciones.crearPublicacion(peticion, respuesta)
+})
+
+router.delete('/publicaciones/:id:id/:titulo/:autores/:fecha', (peticion, respuesta) => {
+    controladorPublicaciones.borrarPublicacion(peticion, respuesta)
+})
+
+router.get('/publicaciones/:id:id/:titulo/:autores/:fecha', (peticion, respuesta) => {
+    controladorPublicaciones.obtenerPublicacion(peticion, respuesta)
+})
+
 module.exports = router
