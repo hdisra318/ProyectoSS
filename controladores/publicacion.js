f5f067c8e851e83a9bc0fab72b900b10325ab25d
dimoc666 <diegomartinez@ciencias.unam.mx>
2023-03-24 19:39:25 -0600

    Se agregaron al controlador funciones de editar

--- controladores/publicacion.js
+++ controladores/publicacion.js
@@ -41,5 +41,32 @@ module.exports = {
             let pub = publicaciones[peticion.params.id, peticion.params.titulo, peticion.params.autores, peticion.params.fecha]
             respuesta.render('publicacion', {publicacion: pub})
         }
+    },
+
+    editarPublicacionTitulo: (peticion, nuevoTitulo, respuesta) => {
+
+        publicaciones[peticion.params.id, peticion.params.titulo, peticion.params.autores, peticion.params.fecha] = modeloPublicacion(
+            peticion.params.id, nuevoTitulo, peticion.params.autores, peticion.params.fecha
+        )
+        let urlNuevaPublicacion = generarUrl(peticion.id, nuevoTitulo, peticion.params.autores, peticion.params.fecha)
+        respuesta.status(201).send(urlNuevaPublicacion)
+    },
+
+    editarPublicacionAutor: (peticion, nuevoAutor, respuesta) => {
+
+        publicaciones[peticion.params.id, peticion.params.titulo, peticion.params.autores, peticion.params.fecha] = modeloPublicacion(
+            peticion.params.id, peticion.params.titulo, nuevoAutor, peticion.params.fecha
+        )
+        let urlNuevaPublicacion = generarUrl(peticion.id, peticion.params.titulo, nuevoAutor, peticion.params.fecha)
+        respuesta.status(201).send(urlNuevaPublicacion)
+    },
+
+    editarPublicacionFecha: (peticion, nuevaFecha, respuesta) => {
+
+        publicaciones[peticion.params.id, peticion.params.titulo, peticion.params.autores, peticion.params.fecha] = modeloPublicacion(
+            peticion.params.id, peticion.params.titulo, peticion.params.autores, nuevaFecha
+        )
+        let urlNuevaPublicacion = generarUrl(peticion.id, nuevoAutor, peticion.params.autores, peticion.params.fecha)
+        respuesta.status(201).send(urlNuevaPublicacion)
     }
 }
