1c3d71ae18a3c673176509cea0ce2333c2a4f71e
dimoc666 <diegomartinez@ciencias.unam.mx>
2023-03-26 11:31:32 -0600

    Se cambio a acceder por titulo

--- controladores/publicacion.js
+++ controladores/publicacion.js
@@ -17,29 +17,30 @@ function generarUrlBorrado(tituloPub, editorialPub, autoresPub, fechaPub) {
 module.exports = {
 
     crearPublicacion: (peticion, respuesta) => {
-        if (publicaciones[peticion.params.id]) {
+        if (publicaciones[peticion.params.titulo]) {
             respuesta.status(409).json({
-                name: 'Id existente',
-                message: `Ya existe publicacion con id: ${peticion.params.id}`
+                name: 'Titulo existente',
+                message: `Ya existe publicacion con titulo: ${peticion.params.titulo}`
             })
             return
         }
-        publicaciones[peticion.params.id] = modeloPublicacion(
-            peticion.params.id, peticion.params.titulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha
+        publicaciones[peticion.params.titulo] = modeloPublicacion(
+            idC, peticion.params.titulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha
         )
-        let urlNuevaPublicacion = generarUrlRegistro(peticion.params.titulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha)
+        ++idC
+        let urlNuevaPublicacion = generarUrlRegistro(peticion.params.titulo)
         respuesta.status(201).send(urlNuevaPublicacion)
     },
 
     borrarPublicacion: (peticion, respuesta) => {
-        delete publicaciones[peticion.params.id]
-        let urlPublicacionBorrada = generarUrlBorrado(peticion.params.titulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha)
+        delete publicaciones[peticion.params.titulo]
+        let urlPublicacionBorrada = generarUrlBorrado(peticion.params.titulo)
         respuesta.send(urlPublicacionBorrada)
     },
 
     obtenerPublicacion: (peticion, respuesta) => {
 
-        if(!publicaciones[peticion.params.titulo, peticion.params.autores, peticion.params.fecha]) {
+        if(!publicaciones[peticion.params.titulo]) {
 
             respuesta.status(404).json({
                 name: 'Publicacion inexistente',
@@ -47,44 +48,48 @@ module.exports = {
                 título ${peticion.params.titulo}`
             })
 
-            let pub = publicaciones[peticion.params.titulo, peticion.params.autores, peticion.params.fecha]
+            let pub = publicaciones[peticion.params.titulo]
             respuesta.render('publicacion', {publicacion: pub})
         }
     },
 
     editarPublicacionTitulo: (peticion, nuevoTitulo, respuesta) => {
-
-        publicaciones[peticion.params.id] = modeloPublicacion(
-            peticion.params.id, nuevoTitulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha
+        delete publicaciones[peticion.params.titulo]
+        publicaciones[nuevoTitulo] = modeloPublicacion(
+            idC, nuevoTitulo, peticion.params.editorial, peticion.params.autores, peticion.params.fecha
         )
-        let urlNuevaPublicacion = generarUrl(peticion.id)
+        ++idC
+        let urlNuevaPublicacion = generarUrl(peticion.titulo)
         respuesta.status(201).send(urlNuevaPublicacion)
     },
 
     editarPublicacionAutor: (peticion, nuevoAutor, respuesta) => {
 
-        publicaciones[peticion.params.id] = modeloPublicacion(
-            peticion.params.id, peticion.params.titulo, peticion.params.editorial, nuevoAutor, peticion.params.fecha
+        publicaciones[peticion.params.titulo] = modeloPublicacion(
+            idC, peticion.params.titulo, peticion.params.editorial, nuevoAutor, peticion.params.fecha
         )
-        let urlNuevaPublicacion = generarUrl(peticion.params.id)
+        ++idC
+        let urlNuevaPublicacion = generarUrl(peticion.params.titulo)
         respuesta.status(201).send(urlNuevaPublicacion)
     },
 
     editarPublicacionEditorial: (peticion, nuevaEditorial, respuesta) => {
 
-        publicaciones[peticion.params.id] = modeloPublicacion(
-            peticion.params.id, peticion.params.titulo, nuevaEditorial, peticion.params.autores, peticion.params.fecha
+        publicaciones[peticion.params.titulo] = modeloPublicacion(
+            idC, peticion.params.titulo, nuevaEditorial, peticion.params.autores, peticion.params.fecha
         )
-        let urlNuevaPublicacion = generarUrl(peticion.params.id)
+        ++idC
+        let urlNuevaPublicacion = generarUrl(peticion.params.titulo)
         respuesta.status(201).send(urlNuevaPublicacion)
     },
 
     editarPublicacionFecha: (peticion, nuevaFecha, respuesta) => {
 
-        publicaciones[peticion.params.id] = modeloPublicacion(
-            peticion.params.id, peticion.params.titulo, peticion.params.editorial, peticion.params.autores, nuevaFecha
+        publicaciones[peticion.params.titulo] = modeloPublicacion(
+            idC, peticion.params.titulo, peticion.params.editorial, peticion.params.autores, nuevaFecha
         )
-        let urlNuevaPublicacion = generarUrl(peticion.params.id)
+        ++idC
+        let urlNuevaPublicacion = generarUrl(peticion.params.titulo)
         respuesta.status(201).send(urlNuevaPublicacion)
     }
 }
